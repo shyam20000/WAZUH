2a2d5e7b3fcfd34ffe680468bcd29503af3533a7
Gastón Palomeque <gaston.palomeque@wazuh.com>
2023-08-15 12:47:30 -0300

    Add field parameter to skip filters based on fields and values

--- framework/wazuh/core/agent.py
+++ framework/wazuh/core/agent.py
@@ -199,7 +199,7 @@ class WazuhDBQueryAgents(WazuhDBQuery):
                                 'separator': 'AND' if len(value) <= 1 or len(value) == i + 1 else 'OR',
                                 'level': 0 if i == len(value) - 1 else 1}
                                for name, value in legacy_filters_as_list.items()
-                               for i, subvalue in enumerate(value) if not self._pass_filter(subvalue)]
+                               for i, subvalue in enumerate(value) if not self._pass_filter(name, subvalue)]
 
         if self.query_filters:
             # if only traditional filters have been defined, remove last AND from the query.
@@ -360,7 +360,7 @@ class WazuhDBQueryGroup(WazuhDBQuery):
                                 'separator': 'AND' if len(value) <= 1 or len(value) == i + 1 else 'OR',
                                 'level': 0 if i == len(value) - 1 else 1}
                                for name, value in legacy_filters_as_list.items()
-                               for i, subvalue in enumerate(value) if not self._pass_filter(subvalue)]
+                               for i, subvalue in enumerate(value) if not self._pass_filter(name, subvalue)]
 
         if self.query_filters:
             # if only traditional filters have been defined, remove last AND from the query.
